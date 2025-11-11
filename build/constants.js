f25c40718b52686e91d93e497d54c652be260b07
Ramachandran A G <ramacg@microsoft.com>
2025-11-11 17:23:33 +0530

    * Update for tests

--- build/constants.js
+++ build/constants.js
@@ -1,4 +1,5 @@
 const path = require('path');
 exports.ExtensionRootDir = path.dirname(__dirname);
 exports.isWindows = /^win/.test(process.platform);
-exports.isCI = process.env.TF_BUILD !== undefined;
+exports.isCI =
+    process.env.TF_BUILD !== undefined || process.env.CI !== undefined || process.env.GITHUB_ACTIONS !== undefined;
